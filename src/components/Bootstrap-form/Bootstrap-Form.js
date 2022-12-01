5597935b836dc21258011355970a3550894362c5
sadmanador <107940313+GaziTasnuvaFarhana@users.noreply.github.com>
2022-12-01 21:00:53 +0600

    module 57-5 done!

--- src/components/Bootstrap-form/Bootstrap-Form.js
+++ src/components/Bootstrap-form/Bootstrap-Form.js
@@ -2,38 +2,55 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import app from '../../firebase/firebase.init'
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from 'react';
 
 const auth = getAuth(app);
 
-const handleFormSubmit = event => {
-    event.preventDefault();
-    const email = event.target.email.value;
-    const password = event.target.password.value;
-    console.log(email, password);
-    createUserWithEmailAndPassword(auth, email, password)
-    .then(result => {
-        const user = result.user;
-        console.log(user);
-    })
-    .catch(error =>{
-        console.error('error: ', error)
-    })
-}
-
 function BootstrapForm() {
+    const [passwordError, setPasswordError] = useState('');
+
+    const handleFormSubmit = event => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        if (!/(?=.*[A-Z].*[A-z])/.test(password)) {
+            setPasswordError('Please provide at least tow capital latter');
+            return
+        }
+        if (password < 6) {
+            setPasswordError('Password should be at least 6 character')
+            return;
+        }
+        if (!/(?=.*[!@#$%^&*])/.test(password)) {
+            setPasswordError('Please have at least one special character')
+            return;
+        }
+        setPasswordError('');
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => {
+                console.error('error: ', error)
+            })
+    }
+
     return (
         <div className="w-50 mx-auto">
             <h3 className='text-warning'>Please register</h3>
             <Form onSubmit={handleFormSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" name='email' placeholder="Enter email" required/>
+                    <Form.Control type="email" name='email' placeholder="Enter email" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name='password' placeholder="Password" required/>
+                    <Form.Control type="password" name='password' placeholder="Password" required />
                 </Form.Group>
+                <p className='text-danger'>{passwordError}</p>
                 <Button variant="primary" type="submit">
                     Register
                 </Button>
